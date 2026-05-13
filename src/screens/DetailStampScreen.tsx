b56796ee1cb3efb6b2a9519b0ec7f1e485d954e8
Lucy Roop <larghmi@gmail.com>
2026-05-13 12:08:15 -0400

    Resolved pre-merging issue.

---
+++ src/screens/DetailStampScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SKILLS_TAXONOMY } from "../config/skillsTaxonomy";
+
+export default function DetailStampScreen(){
+    return(
+        
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
